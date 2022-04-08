ba48d1b1ad5d4f4ec85b6d170f8616cea977922c
saikrishnna29 <97147477+saikrishnna29@users.noreply.github.com>
2022-04-09 01:24:58 +0530

    Update main.tf

--- nginx-provisioner/main.tf
+++ nginx-provisioner/main.tf
@@ -6,12 +6,16 @@ provider "aws" {
 
 resource "aws_vpc" "vpc" {
     cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "nginx-vpc"
+    }
 }
 
 resource "aws_internet_gateway" "main" {
     vpc_id = aws_vpc.vpc.id
     tags = {
-        Name = "nginx-vpc"
+        Name = "nginx-igw"
     }
 }
 
